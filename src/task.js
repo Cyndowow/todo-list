c03607706a7d0c093cc390a6123a105eab85d610
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-04-03 11:08:28 +0200

    Add project, task, todolist classes

---
+++ src/task.js
@@ -0,0 +1,37 @@
+export default class Task {
+    constructor(name, details, dueDate = "No date") {
+        this.name = name;
+        this.dueDate = dueDate;
+    }
+
+    setName(name) {
+        this.name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    getDate() {
+        return this.dueDate;
+    }
+
+    getDateFormatted() {
+        const day = this.dueDate.split("/")[0];
+        const month = this.dueDate.split("/")[1];
+        const year = this.dueDate.split("/")[2];
+        return `${day}.${month}.${year}`;
+    }
+
+    setDetails(details) {
+        this.details = details;
+    }
+
+    getDetails() {
+        return this.details;
+    }
+}
